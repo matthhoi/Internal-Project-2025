53eb181edd71e341bfddb9aa302580fd66daaeab
matthhoi <smithm4@stu.otc.school.nz>
2025-05-21 09:59:05 +1200

    creation of the menu changer
    added the functions for the sub and main windows and created a way to switch between them

--- mian.py
+++ mian.py
@@ -16,12 +16,44 @@ bg_color = "#5c9ead"
 label_color = "#236273"
 white = "#ffffff"
 exit = False
+login = False
 is_visable = "*"
+menu = 1
 
 # functions
 def error_message(msg):
     """Display an error message."""
     messagebox.showerror("Error", msg)
+
+def main_menu():
+    """Display the main menu."""
+    global exit
+    while exit == False:
+        # Create the main menu window
+        window = tk.Tk()
+        window.title("Login Window")
+        window.geometry("1000x650")
+        window.config(bg=bg_color)
+        window.resizable(width=False, height=False)
+        window.mainloop()
+
+        # exit the program if the user clicks the exit button
+        exit = messagebox.askokcancel("Exit", "Are you sure you want to exit?")
+
+def sub_menu():
+    """Display the sub menu."""
+    global exit
+    while exit == False:
+        # Create the main menu window
+        window = tk.Tk()
+        window.title("Login Window")
+        window.geometry("1000x650")
+        window.config(bg=bg_color)
+        window.resizable(width=False, height=False)
+        window.mainloop()
+
+        # exit the program if the user clicks the exit button
+        exit = messagebox.askokcancel("Exit", "Are you sure you want to exit?")
 
 def show_password():
     """show the password"""
@@ -35,8 +67,21 @@ def show_password():
         password_entry.config(show=is_visable)
         show_password.config(text="Show Password")
 
-def login():
+def menu_chager():
+    """Change the menu"""
+    global menu
+    if menu == 1:
+        # call the main menu
+        menu = 2
+        main_menu()
+    else:
+        #call the sub menu
+        menu = 1
+        sub_menu()
+
+def cheek_login():
     """Check if the username and password are correct."""
+    global login
     with sqlite3.connect(DATABASE) as d_b:
         # Check if the username and password are correct
         cursor = d_b.cursor()
@@ -48,57 +93,61 @@ def login():
             messagebox.showinfo("Login", 
                                 f"Login successful! \n Welcome {results[0][0]}")
             window.destroy()
-            # Call the main function here
+            login = True
         else:
             messagebox.showerror("Login", "Invalid username or password.")
             username_entry.delete(0, tk.END)
             password_entry.delete(0, tk.END)
 
-# login window
-while exit == False:
-    while True:
-        try:
-            # Connect to the database
-            conn = sqlite3.connect(DATABASE)
-            cursor = conn.cursor()
-            break
-        except sqlite3.Error as e:
-            error_message(f"Database connection error: {e}")
-            break
+while __name__ == "__main__":
+    """Create the login window."""
+    # login window
+    while login == False:
+        # Create the login window
+        window = tk.Tk()
+        window.title("Login Window")
+        window.geometry("1000x650")
+        window.config(bg=bg_color)
+        window.resizable(width=False, height=False)
 
-    # Create the login window
-    window = tk.Tk()
-    window.title("Login Window")
-    window.geometry("1000x650")
-    window.config(bg=bg_color)
-    window.resizable(width=False, height=False)
+        # Create the mian Frame
+        frame = tk.Frame(master=window, bg=label_color)
+        frame.place(x=200, y=25, width=600, height=600)
 
-    # Create the mian Frame
-    frame = tk.Frame(master=window, bg=label_color)
-    frame.place(x=200, y=25, width=600, height=600)
+        # show password button
+        show_password = tk.Button(frame, text="Show Password", 
+                                  font=('Arial', 12), bg=white, 
+                                  command=show_password)
+        show_password.place(x=350, y=300)
 
-    # show password button
-    show_password = tk.Button(frame, text="Show Password", font=('Arial', 12), 
-                              bg=white, command=show_password)
-    show_password.place(x=350, y=300)
+        # Create the login feilds
+        username_label = tk.Label(frame, text="Username", 
+                                  font=('Arial',15,"bold"), bg=label_color) 
+        username_entry = tk.Entry(frame, width=75)
+        password_label = tk.Label(frame, text="Password", 
+                                  font=('Arial',15,"bold"), bg=label_color)
+        password_entry = tk.Entry(frame, show=is_visable, width=75)
+        username_entry.place(x=25, y=130)
+        username_label.place(x=50, y=100)
+        password_entry.place(x=25, y=230)
+        password_label.place(x=50, y=200)
+            
+        # Create the login button
+        login_button = tk.Button(frame, text="login", 
+                                 font=('Arial', 15,"bold"), bg=white, 
+                                 width=10, height=2, command=cheek_login)
+        login_button.place(x=225, y=400)
+        window.mainloop()
 
-    # Create the login feilds
-    username_label = tk.Label(frame, text="Username", font=('Arial',15,"bold"), 
-                              bg=label_color) 
-    username_entry = tk.Entry(frame, width=75)
-    password_label = tk.Label(frame, text="Password", font=('Arial',15,"bold"), 
-                              bg=label_color)
-    password_entry = tk.Entry(frame, show=is_visable, width=75)
-    username_entry.place(x=25, y=130)
-    username_label.place(x=50, y=100)
-    password_entry.place(x=25, y=230)
-    password_label.place(x=50, y=200)
+        if login == False:
+            # exit the program if the user clicks the exit button
+            exit = messagebox.askokcancel("Exit", "Are you sure you want to exit?")
+            login = exit
     
-    # Create the login button
-    login_button = tk.Button(frame, text="login", font=('Arial', 15,"bold"), 
-                             bg=white, width=10, height=2, command=login)
-    login_button.place(x=225, y=400)
-    window.mainloop()
+    while True:
+        if exit == True:
+            break
+        # call the menu changer
+        menu_chager()
 
-    # exit the program if the user clicks the exit button
-    exit = messagebox.askokcancel("Exit", "Are you sure you want to exit?")
+    break
